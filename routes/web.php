c65f4184c0e66da15bd32659ed613a94bb5bd65a
furu-tuan-ngo <tuango.2104200@gmail.com>
2020-12-18 21:20:53 +0700

    setup to server

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('master');
+Route::get("/", function (){
+    return view("welcome");
 });
-
-Route::get('authenticate-failure','AuthenticationController@failure')->name('login');
+Route::get('authenticate-failure', 'AuthenticationController@failure')->name('login');
