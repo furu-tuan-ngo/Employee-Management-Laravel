869ce5ad77f23f765b7e20f8b1c7bd5b76c1b5a4
furu-tuan-ngo <tuango.2104200@gmail.com>
2020-12-25 06:17:13 +0700

    fix bug user table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,7 +21,6 @@ class CreateUsersTable extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
-            $table->foreign('ma_phan_quyen')->references('id')->on('phan_quyen')->onUpdate('cascade')->onDelete('cascade');
         });
     }
 
